17180988d7725a17b08058dcec0f8814e026b5c6
Chris Gray <chrismgray@gmail.com>
2011-10-29 22:35:05 -0600

    A new function is starting.

--- voronoi-diagram/src/voronoi_diagram/rect.clj
+++ voronoi-diagram/src/voronoi_diagram/rect.clj
@@ -1,5 +1,5 @@
 (ns voronoi-diagram.rect
-  (require [voronoi-diagram.seg :as seg]))
+  (:require [voronoi-diagram.seg :as seg]))
 
 (defn update [which rect val]
   (assoc rect which val))
@@ -15,6 +15,11 @@
 
 (defn new-rect [x1 y1 x2 y2]
   {:x1 x1 :x2 x2 :y1 y1 :y2 y2})
+
+(defn first-sites [left-rec right-rec]
+  (let [x1-site (right-rec :x1-site)
+        x2-site (left-rec :x2-site)]
+    [x1-site x2-site]))
 
 (defn next-site [r1 r2 new-pt]
   (let [new-seg (filter (partial seg/pt-on-seg? new-pt) (concat r1 r2))]
---
+++ voronoi-diagram/test/voronoi_diagram/test/rect.clj
@@ -0,0 +1,8 @@
+(ns voronoi-diagram.test.rect
+  (:use [clojure.test])
+  (:require [voronoi-diagram.rect :as rect]
+            [voronoi-diagram.seg :as seg]
+            [voronoi-diagram.pt :as pt]))
+
+(deftest first-sites
+)
