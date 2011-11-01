744d34b6d690c96d0ed46edaec35fb4a10aad4b1
Chris Gray <chrismgray@gmail.com>
2011-11-01 14:15:21 -0600

    Finally got merge-rects to pass a simple test

--- voronoi-diagram/src/voronoi_diagram/rect.clj
+++ voronoi-diagram/src/voronoi_diagram/rect.clj
@@ -8,19 +8,40 @@
 (def update-x2 (partial update :x2))
 (def update-x1-site (partial update :x1-site))
 (def update-x2-site (partial update :x2-site))
-
-(defn update-all-corners [rect site]
-  (-> rect 
-      (update-x1-site site)
-      (update-x2-site site)))
-
-(defn new-rect [x1 y1 x2 y2]
-  {:x1 x1 :x2 x2 :y1 y1 :y2 y2})
+(def update-regions (partial update :regions))
 
 (defn new-region [& coords]
   (map #(seg/new-seg %1 %2)
        (map (fn [[x y]] (pt/new-pt x y)) coords)
        (map (fn [[x y]] (pt/new-pt x y)) (conj (vec (next coords)) (first coords)))))
+
+(defn update-all-corners [rect site]
+  (-> rect 
+      (update-x1-site site)
+      (update-x2-site site)
+      (update-regions {site (new-region [(rect :x1) (rect :y1)]
+                                        [(rect :x1) (rect :y2)]
+                                        [(rect :x2) (rect :y2)]
+                                        [(rect :x2) (rect :y1)])})))
+(defn new-rect [x1 y1 x2 y2]
+  {:x1 x1 :x2 x2 :y1 y1 :y2 y2})
+
+(defn find-highest-bisector-lower-than [s1 s2 left-rec right-rec prev-pt]
+  (let [bounding-box-x (left-rec :x2)
+        possibilities (if (= (prev-pt :x) (left-rec :x1))
+                        (remove nil?
+                                (map #(seg/find-bisector %1 s2 %2 (get-in right-rec [:regions s2]) bounding-box-x)
+                                     (keys (left-rec :regions))
+                                     (vals (left-rec :regions))))
+                        (remove nil?
+                                (map #(seg/find-bisector s1 %1 (get-in left-rec [:regions s1]) %2 bounding-box-x)
+                                     (keys (right-rec :regions))
+                                     (vals (right-rec :regions)))))
+        possibilities-lower-than (filter #(< (get-in % [0 :e1 :y]) (prev-pt :y)) possibilities)]
+    (if (empty? possibilities-lower-than)
+      (pt/new-pt (if (= (prev-pt :x) (left-rec :x1)) (left-rec :x1) (right-rec :x2)) (left-rec :y2))
+      (let [highest-seg (apply max-key #(get-in % [0 :e1 :y]) (filter #(< (get-in % [0 :e1 :y]) (prev-pt :y)) possibilities))]
+        (get-in highest-seg [0 :e1])))))
 
 (defn first-segs [left-rec right-rec]
   (let [x1-site (right-rec :x1-site)
@@ -53,7 +74,7 @@
                                                                 %2
                                                                 split-line-x) (keys (right-rec :regions)) (vals (right-rec :regions))))
             highest-seg (apply max-key #(get-in % [1 :e2 :y]) possibilities)]
-        (if (= split-line-y (get-in highest-seg [0 :e2 :y]))
+        (if (= split-line-y (get-in highest-seg [1 :e2 :y]))
           (->>  possibilities
                 (filter #(= split-line-y (get-in % [1 :e2 :y])))
                 (apply min-key #(get-in % [1 :e2 :x])))
@@ -67,47 +88,132 @@
         split-line-x (right-rec :x1)
         split-line-y (right-rec :y1)
         split-line-pt (pt/new-pt split-line-x split-line-y)
-        first-segs (first-segs left-rec right-rec)]
-    (if (< (pt/sq-dist x1-site split-line-pt) (pt/sq-dist x2-site split-line-pt))
-      [((first first-segs) :neighbor) x1-site]
-      [x2-site ((second first-segs) :neighbor)])))
+        first-segs (first-segs left-rec right-rec)
+        first-pt (get-in first-segs [0 :e1])]
+    (if (not= (first-pt :y) (left-rec :y1))
+      (if (< (pt/sq-dist x1-site split-line-pt) (pt/sq-dist x2-site split-line-pt))
+        [nil x1-site (pt/new-pt (left-rec :x1) (left-rec :y1))]
+        [x2-site nil (pt/new-pt (right-rec :x2) (right-rec :y1))])
+      (if (< (pt/sq-dist x1-site split-line-pt) (pt/sq-dist x2-site split-line-pt))
+        [((first first-segs) :neighbor) x1-site]
+        [x2-site ((second first-segs) :neighbor)]))))
 
 (defn next-site [r1 r2 new-pt]
   (let [new-seg (first (filter (partial seg/pt-on-seg? new-pt) (concat r1 r2)))]
-    (new-seg :neighbor)))
+    (if (nil? new-seg)
+      nil
+      (new-seg :neighbor))))
 
 (defn next-sites [s1 s2 left-rec right-rec new-pt]
   (let [next-site (next-site (get-in left-rec [:regions s1])
                              (get-in right-rec [:regions s2]) new-pt)]
-    (if (nil? (get-in left-rec [:regions next-site]))
-      [s1 next-site]
-      [next-site s2])))
+    (if (= (new-pt :y) (left-rec :y2))
+      [nil nil]
+      (if (nil? next-site)
+        (if (= (new-pt :x) (left-rec :x1))
+          [nil s2]
+          [s1 nil])
+        (if (nil? (get-in left-rec [:regions next-site]))
+          [s1 next-site]
+          [next-site s2])))))
 
 (defn sites-list
   ([left-rec right-rec]
-     (let [[s1 s2] (first-sites left-rec right-rec)]
-       (sites-list s1 s2 left-rec right-rec)))
-  ([s1 s2 left-rec right-rec]
-     (if (or (nil? s1) (nil? s2))
+     (let [[s1 s2 first-pt] (first-sites left-rec right-rec)]
+       (sites-list s1 s2 left-rec right-rec first-pt)))
+  ([s1 s2 left-rec right-rec prev-pt]
+     (if (and (nil? s1) (nil? s2))
        nil
        (let [r1 (get-in left-rec [:regions s1])
              r2 (get-in right-rec [:regions s2])
              bounding-box-x (left-rec :x2)
-             new-pt (get-in (seg/find-bisector s1 s2 r1 r2 bounding-box-x) [0 :e2])
+             new-pt (if (and s1 s2)
+                      (get-in (seg/find-bisector s1 s2 r1 r2 bounding-box-x) [0 :e2])
+                      (find-highest-bisector-lower-than s1 s2 left-rec right-rec prev-pt))
              [next-s1 next-s2] (next-sites s1 s2 left-rec right-rec new-pt)]
-         (lazy-seq (cons [s1 s2] (sites-list next-s1 next-s2 left-rec right-rec)))))))
+         (lazy-seq (cons [s1 s2] (sites-list next-s1 next-s2 left-rec right-rec new-pt)))))))
+
+(defn insinuate-segs [segs region]
+  (let [e1 ((first segs) :e1)
+        e2 ((last segs) :e2)
+        good-segs (->> (lazy-cat region region)
+                       (drop-while (complement (partial seg/pt-on-line? e2)))
+                       (take-while (complement (partial seg/pt-on-line? e1))))
+        e1-seg (first (drop-while (complement (partial seg/pt-on-line? e1)) region))
+        e2-seg (first good-segs)
+        prev-seg (seg/new-seg (e1-seg :e1) ((first segs) :e1) (e1-seg :neighbor))
+        next-seg (seg/new-seg ((last segs) :e2) (e2-seg :e2) (e2-seg :neighbor))]
+    (concat (rest good-segs) (list prev-seg) segs (list next-seg))))
 
 (defn insinuate-seg [seg region]
-  (let [e1 (seg :e1)
-        e2 (seg :e2)
-        good-segs (->> (lazy-cat region region)
-                       (drop-while (complement (partial seg/pt-on-seg? e2)))
-                       (take-while (complement (partial seg/pt-on-seg? e1))))
-        e1-seg (first (drop-while (complement (partial seg/pt-on-seg? e1)) region))
-        e2-seg (first good-segs)
-        prev-seg (seg/new-seg (e1-seg :e1) (seg :e1) (e1-seg :neighbor))
-        next-seg (seg/new-seg (seg :e2) (e2-seg :e2) (e2-seg :neighbor))]
-    (concat (rest good-segs) (list prev-seg seg next-seg))))
+  (insinuate-segs (list seg) region))
 
-(defn merge-rects [left-rec right-rec])
+(defn merge-helper [[m left-rec right-rec] [s1 s2]]
+  (let [r1 (get-in left-rec [:regions s1])
+        r2 (get-in right-rec [:regions s2])
+        bounding-box-x (left-rec :x2)]
+    (cond (nil? s1)
+          [(assoc m s2 (conj (get m s2 []) nil)) [left-rec right-rec]]
+          (nil? s2)
+          [(assoc m s1 (vec (cons nil (get m s1 [])))) [left-rec right-rec]]
+          :else
+          (let [[down-bisector up-bisector] (seg/find-bisector s1 s2 r1 r2 bounding-box-x)]
+           [(-> m
+                (assoc s1 (vec (cons up-bisector (get m s1 []))))
+                (assoc s2 (conj (get m s2 []) down-bisector))) left-rec right-rec]))))
+
+(defn bisector-path-map-fix
+  "Some of the segments in the bisector-path-map might be nil.
+   We are guaranteed that at most one in a row is, so we fix the
+   nil segments by looking at the next or previous segment."
+  [[b-p-m left-rec right-rec] key]
+  (vector
+   (update-in b-p-m [key]
+              #(vec
+                (map (fn [prev curr next-seg]
+                       (cond (not (nil? curr))
+                             curr
+                             (and (not (nil? prev)) (not (nil? next-seg)))
+                             (seg/new-seg (prev :e2) (next-seg :e1))
+                             (and (nil? prev) (not (nil? next-seg)))
+                             (seg/new-seg (pt/new-pt (get-in next-seg [:e1 :x])
+                                                     (if (< (get-in next-seg [:e1 :y])
+                                                            (get-in next-seg [:e2 :y]))
+                                                       (left-rec :y2)
+                                                       (left-rec :y1)))
+                                          (next-seg :e1))
+                             (and (not (nil? prev)) (nil? next-seg))
+                             (seg/new-seg (prev :e2) (pt/new-pt
+                                                      (get-in prev [:e2 :x])
+                                                      (if (< (get-in prev [:e1 :y])
+                                                             (get-in prev [:e2 :y]))
+                                                        (left-rec :y1)
+                                                        (left-rec :y2))))
+                             :else
+                             (assert false)))
+                     (cons nil %)       ; segs with nil in front
+                     %                  ; segs
+                     (conj % nil)       ; segs with nil in back
+                     ))) left-rec right-rec))
+
+(defn region-insinuator [rec [site segs]]
+  (assoc-in rec [:regions site] (insinuate-segs segs (get-in rec [:regions site]))))
+
+(defn merge-rects [left-rec right-rec]
+  (let [sites-to-consider (sites-list left-rec right-rec)
+        rec (new-rect (left-rec :x1) (left-rec :y1) (right-rec :x2) (right-rec :y2))
+        [bisector-path-map _ _] (reduce merge-helper [{} left-rec right-rec] sites-to-consider)
+        [bisector-path-map _ _] (reduce bisector-path-map-fix [bisector-path-map left-rec right-rec] (keys bisector-path-map))
+        rec (assoc rec :regions (merge (left-rec :regions) (right-rec :regions)))
+        rec (reduce region-insinuator rec bisector-path-map)
+        first-seg (first (first-segs left-rec right-rec))
+        new-x1-site (if (= (get-in [:e1 :x] first-seg) (left-rec :x1))
+                      (right-rec :x1-site)
+                      (left-rec :x1-site))
+        new-x2-site (if (= (get-in [:e1 :x] first-seg) (right-rec :x2))
+                      (left-rec :x2-site)
+                      (right-rec :x2-site))]
+    (assoc rec
+      :x1-site new-x1-site
+      :x2-site new-x2-site)))
 
--- voronoi-diagram/test/voronoi_diagram/test/rect.clj
+++ voronoi-diagram/test/voronoi_diagram/test/rect.clj
@@ -42,16 +42,16 @@
         site1 (pt/new-pt -7 6)
         site2 (pt/new-pt -5 4)
         site3 (pt/new-pt 1 6)
-        r1 (rect/new-region [-1 10] [-15 10] [-15 -3])
-        r2 (rect/new-region [-1 10] [-15 -3] [-15 -10] [0 -10] [0 10])
+        r1 (rect/new-region [-1 10] [-15 10] [-15 -8])
+        r2 (rect/new-region [-1 10] [-15 -8] [-15 -10] [0 -10] [0 10])
         r3 (rect/new-region [0 10] [0 -10] [15 -10] [15 10])
-        neighbor-seg (seg/new-seg (pt/new-pt -15 -3) (pt/new-pt -1 10) site2)
+        neighbor-seg (seg/new-seg (pt/new-pt -15 -8) (pt/new-pt -1 10) site2)
         r1 (assoc-in (vec r1) [2] neighbor-seg)
         left-rec (assoc left-rec :x1-site site1 :x2-site site2
                         :regions {site1 r1 site2 r2})
         right-rec (assoc right-rec :x1-site site3 :x2-site site3
                          :regions {site3 r3})]
-    (is (= [[site1 site3] [site2 site3]]
+    (is (= [[site1 site3] [site2 site3] [site2 nil]]
            (vec (rect/sites-list left-rec right-rec))))))
 
 (deftest insinuate-seg
@@ -77,3 +77,35 @@
         seg (seg/new-seg (pt/new-pt 1 0) (pt/new-pt 2 1))]
     (is (= (rect/new-region [1 1] [0 0] [1 0] [2 1])
            (rect/insinuate-seg seg region)))))
+
+(deftest insinuate-segs
+  (let [region (rect/new-region [0 0] [3 0] [4 1] [1 1])
+        segs [(seg/new-seg (pt/new-pt 4 0) (pt/new-pt 4.5 0.5))
+              (seg/new-seg (pt/new-pt 4.5 0.5) (pt/new-pt 5 1))]]
+    (is (= (rect/new-region [1 1] [0 0] [4 0] [4.5 0.5] [5 1])
+           (rect/insinuate-segs segs region)))))
+
+(deftest first-segs-2
+  (let [left-rec (rect/new-rect -12 8 -5 -8)
+        right-rec (rect/new-rect -5 8 0 -8)
+        s1 (pt/new-pt -6 5)
+        s2 (pt/new-pt -4 3)
+        left-rec (rect/update-all-corners left-rec s1)
+        right-rec (rect/update-all-corners right-rec s2)]
+    (is (= 2 (count (rect/first-segs left-rec right-rec))))))
+
+(deftest merge-rects
+  (let [left-rec (rect/new-rect -12 8 -5 -8)
+        right-rec (rect/new-rect -5 8 0 -8)
+        s1 (pt/new-pt -6 5)
+        s2 (pt/new-pt -4 3)
+        left-rec (rect/update-all-corners left-rec s1)
+        right-rec (rect/update-all-corners right-rec s2)
+        merged-rec (rect/merge-rects left-rec right-rec)]
+    (is (and
+         (= -12 (merged-rec :x1))
+         (= 8 (merged-rec :y1))
+         (= 0 (merged-rec :x2))
+         (= -8 (merged-rec :y2))
+         (= s1 (merged-rec :x1-site))
+         (= s2 (merged-rec :x2-site))))))
