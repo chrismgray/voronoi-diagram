1a337002fd00d53a03b790e69ff505fdf48d0fd0
Chris Gray <chrismgray@gmail.com>
2011-11-03 15:26:31 -0600

    Fixed some bugs in the core file

--- voronoi-diagram/src/voronoi_diagram/core.clj
+++ voronoi-diagram/src/voronoi_diagram/core.clj
@@ -1,5 +1,6 @@
 (ns voronoi-diagram.core
-  (require [voronoi-diagram.rect :as rect]))
+  (require [voronoi-diagram.rect :as rect]
+           [voronoi-diagram.pt :as pt]))
 
 (defn voronoi-diagram-merge [left-rec right-rec]
   (rect/merge-rects left-rec right-rec))
@@ -8,16 +9,16 @@
   (let [num-sites (count sites)]
     (if (= num-sites 1)
       (rect/update-all-corners bounding-rect (first sites))
-      (let [half-sites (/ num-sites 2)
-            middle-x (/ (+ (get-in sites [half-sites :x])
-                           (get-in sites [(inc half-sites) :x])) 2)
+      (let [half-sites (int (/ num-sites 2))
+            middle-x (/ (+ (get-in sites [(dec half-sites) :x])
+                           (get-in sites [half-sites :x])) 2)
             left-rect (rect/update-x2 bounding-rect middle-x)
             right-rect (rect/update-x1 bounding-rect middle-x)
             [left-split-sites right-split-sites] (split-at half-sites sites)]
-        (voronoi-diagram-merge (voronoi-diagram-divide-conquer left-rect left-split-sites)
-                               (voronoi-diagram-divide-conquer right-rect right-split-sites))))))
+        (voronoi-diagram-merge (voronoi-diagram-divide-conquer left-rect (vec left-split-sites))
+                               (voronoi-diagram-divide-conquer right-rect (vec right-split-sites)))))))
 
 (defn voronoi-diagram [bounding-rect & sites]
-  (let [sorted-sites (sort-by :x sites)
-        diagram (voronoi-diagram-divide-conquer bounding-rect sorted-sites)]
-    diagram))
+  (let [sorted-sites (vec (sort-by :x sites))]
+    (voronoi-diagram-divide-conquer bounding-rect sorted-sites)))
+
